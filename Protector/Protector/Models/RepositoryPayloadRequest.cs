8db5085efd2d2772c9efc2462ccaea1c6f83539e
Brandon Ross <brandon.ross@bkfs.com>
2020-10-03 23:19:04 -0400

    Created a logic layer to handle everything instead of the controller. Updated the appsettings with the fields needed, added method to validate the incoming signature from the webhook.

---
+++ Protector/Protector/Models/RepositoryPayloadRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace Protector.Models
+{
+    public class RepositoryPayloadRequest
+    {
+        public string action { get; set; }
+
+        public Repository repository { get; set; }
+    }
+
+    public class Repository
+    {
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public Owner owner { get; set; }
+    }
+
+    public class Owner
+    {
+        public string login { get; set; }
+    }
+}
